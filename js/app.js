8bb591e76d581b1c41244a5ea8fb6fcc43b95416
Qazi Sami ur Rahman <qsrahmans@gmail.com>
2020-12-05 14:46:25 +0500

    working prototype

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 'use strict'
+
+const panels = document.querySelectorAll(".panel");
+
+for (const panel of panels) {
+  panel.addEventListener("click", function () {
+    activatePanel(panel);
+  });
+}
+
+function activatePanel(panel) {
+  const title = panel.firstElementChild;
+  const panelContent = panel.lastElementChild;
+
+  title.classList.toggle("active");
+
+  if (panelContent.style.maxHeight) {
+    panelContent.style.maxHeight = null;
+    panelContent.style.marginBottom = null;
+  } else {
+    panelContent.style.maxHeight = `${panelContent.scrollHeight}px`;
+    panelContent.style.marginBottom = "1.1rem";
+  }
+}
+
